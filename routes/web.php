5ad24a6e59b01d0092de0f8ec47b4bce70dcba6f
Sanduni Fernando <sandunigfdo@gmail.com>
2024-07-18 08:56:14 +1200

    Add /register endpoint through EnsureAdmin middleware

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\EnsureAdmin;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisteredUserController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +18,10 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::middleware('auth',EnsureAdmin::class)->group(function () {
+    Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
+});
+
 
 require __DIR__.'/auth.php';
