6413eecc541164c7fcce58a80414c99c6a726322
Sanduni Fernando <sandunigfdo@gmail.com>
2024-07-19 09:47:08 +1200

    Remove unwanted code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,10 +21,10 @@
                 @if (Route::has('login'))
                     <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
                         @auth
-                            <a  href="{{ route('posts.index') }}"
-                                class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-blue-500">
-                            Posts
-                            </a>
+{{--                            <a  href="{{ route('posts.index') }}"--}}
+{{--                                class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-blue-500">--}}
+{{--                            Posts--}}
+{{--                            </a>--}}
                         @else
                             <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-blue-500">Log in</a>
 
