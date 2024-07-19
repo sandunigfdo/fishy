c132662e2e1edfd92cfa58b89714a84a13c56c30
Sanduni Fernando <sandunigfdo@gmail.com>
2024-07-19 20:58:07 +1200

    Add a table to display customer data

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,6 +15,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-10">
                     <!-- Customer Management table -->
+
                     <div class="px-4 sm:px-6 lg:px-8">
                         <div class="sm:flex sm:items-center">
                             <div class="sm:flex-auto">
