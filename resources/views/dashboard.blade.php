68c1fd2b601f870c0620ef70e2e265942ac8ab7e
Sanduni Fernando <sandunigfdo@gmail.com>
2024-07-19 20:56:51 +1200

    Create a form to add organization and user data

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,21 +13,18 @@
 {{--                </div>--}}
 {{--            </div>--}}
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6">
-                    <!-- User Management table -->
+                <div class="p-10">
+                    <!-- Customer Management table -->
                     <div class="px-4 sm:px-6 lg:px-8">
                         <div class="sm:flex sm:items-center">
                             <div class="sm:flex-auto">
-                                <h1 class="text-lg font-semibold leading-6 text-gray-900">Users</h1>
-                                <p class="mt-2 text-base text-gray-700">A list of all the users in your account including
-                                    their
-                                    name,
-                                    title, email and role.</p>
+                                <h1 class="text-lg font-semibold leading-6 text-gray-900">Customers</h1>
+                                <p class="mt-2 text-base text-gray-700">A list of all Customers.</p>
                             </div>
                             <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
                                 <button type="button"
                                         class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                                    Add user
+                                    Add Customer
                                 </button>
                             </div>
                         </div>
@@ -42,42 +39,56 @@
                                                 Name
                                             </th>
                                             <th scope="col"
-                                                class="sticky top-0 z-10 hidden border-b border-gray-300 bg-white bg-opacity-75 px-3 py-3.5 text-left text-base font-semibold text-gray-900 backdrop-blur backdrop-filter sm:table-cell">
-                                                Title
-                                            </th>
-                                            <th scope="col"
                                                 class="sticky top-0 z-10 hidden border-b border-gray-300 bg-white bg-opacity-75 px-3 py-3.5 text-left text-base font-semibold text-gray-900 backdrop-blur backdrop-filter lg:table-cell">
                                                 Email
-                                            </th>
-                                            <th scope="col"
-                                                class="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 px-3 py-3.5 text-left text-base font-semibold text-gray-900 backdrop-blur backdrop-filter">
-                                                Role
                                             </th>
                                             <th scope="col"
                                                 class="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-3 pr-4 backdrop-blur backdrop-filter sm:pr-6 lg:pr-8">
                                                 <span class="sr-only">Edit</span>
                                             </th>
+                                            <th scope="col"
+                                                class="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-3 pr-4 backdrop-blur backdrop-filter sm:pr-6 lg:pr-8">
+                                                <span class="sr-only">Edit</span>
+                                            </th>
+
                                         </tr>
                                         </thead>
                                         <tbody>
                                         <tr>
                                             <td class="whitespace-nowrap border-b border-gray-200 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">
-                                                Lindsay Walton
-                                            </td>
-                                            <td class="hidden whitespace-nowrap border-b border-gray-200 px-3 py-4 text-sm text-gray-500 sm:table-cell">
-                                                Front-end Developer
+                                                Ambience
                                             </td>
                                             <td class="hidden whitespace-nowrap border-b border-gray-200 px-3 py-4 text-sm text-gray-500 lg:table-cell">
-                                                lindsay.walton@example.com
+                                                systems@ambience.com
                                             </td>
-                                            <td class="whitespace-nowrap border-b border-gray-200 px-3 py-4 text-sm text-gray-500">
-                                                Member
-                                            </td>
-                                            <td class="relative whitespace-nowrap border-b border-gray-200 py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-8 lg:pr-8">
+                                            <td class="relative whitespace-nowrap border-b border-gray-200 py-4 pl-3 pr-4 text-center text-sm font-medium sm:pr-8 lg:pr-8">
                                                 <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span
-                                                        class="sr-only">, Lindsay Walton</span></a>
+                                                        class="sr-only">, Ambience</span></a>
+                                            </td>
+                                            <td class="relative whitespace-nowrap border-b border-gray-200 py-4 pl-3 pr-4 text-center text-sm font-medium sm:pr-8 lg:pr-8">
+                                                <a href="#" class="text-red-600 hover:text-indigo-900">Delete<span
+                                                        class="sr-only">, Ambience</span></a>
                                             </td>
                                         </tr>
+
+                                        <tr>
+                                            <td class="whitespace-nowrap border-b border-gray-200 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">
+                                                Miniso
+                                            </td>
+                                            <td class="hidden whitespace-nowrap border-b border-gray-200 px-3 py-4 text-sm text-gray-500 lg:table-cell">
+                                                systems@miniso.com
+                                            </td>
+                                            <td class="relative whitespace-nowrap border-b border-gray-200 py-4 pl-3 pr-4 text-center text-sm font-medium sm:pr-8 lg:pr-8">
+                                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span
+                                                        class="sr-only">, Miniso</span></a>
+                                            </td>
+                                            <td class="relative whitespace-nowrap border-b border-gray-200 py-4 pl-3 pr-4 text-center text-sm font-medium sm:pr-8 lg:pr-8">
+                                                <a href="#" class="text-red-600 hover:text-indigo-900">Delete<span
+                                                        class="sr-only">, Miniso</span></a>
+                                            </td>
+                                        </tr>
+
+
 
                                         <!-- More people... -->
                                         </tbody>
@@ -87,7 +98,7 @@
                         </div>
                     </div>
 
-                    <!-- User Management Table -->
+                    <!-- Customer Management Table -->
                 </div>
             </div>
 
