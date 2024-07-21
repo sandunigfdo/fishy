943a9c833d67bd06b940c7bd5625050bfbd2c441
Sanduni Fernando <sandunigfdo@gmail.com>
2024-07-21 14:31:15 +1200

    Remove Company email

--- resources/views/organizations/index.blade.php
+++ resources/views/organizations/index.blade.php
@@ -30,19 +30,10 @@
                                 </div>
 
                                 <div>
-                                    <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Personal Email
+                                    <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email
                                         address</label>
                                     <div class="mt-2">
                                         <input id="email" name="email" type="email" autocomplete="email" required
-                                               class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                                    </div>
-                                </div>
-
-                                 <div>
-                                    <label for="o_email" class="block text-sm font-medium leading-6 text-gray-900">Company Email
-                                        address</label>
-                                    <div class="mt-2">
-                                        <input id="o_email" name="o_email" type="email" autocomplete="email" required
                                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                                     </div>
                                 </div>
