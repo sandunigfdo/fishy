3ab1ec20c6d70dfb0033476d53641610f7540d3c
Sanduni Fernando <sandunigfdo@gmail.com>
2024-07-24 09:35:49 +1200

    Link organizations.index page to Add Customer button & retrieve info from DB

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,7 +15,6 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-10">
                     <!-- Customer Management table -->
-
                     <div class="px-4 sm:px-6 lg:px-8">
                         <div class="sm:flex sm:items-center">
                             <div class="sm:flex-auto">
@@ -23,10 +22,10 @@
                                 <p class="mt-2 text-base text-gray-700">A list of all Customers.</p>
                             </div>
                             <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-                                <button type="button"
-                                        class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                <a href="{{ route('organizations.index') }}"
+                                   class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                                     Add Customer
-                                </button>
+                                </a>
                             </div>
                         </div>
                         <div class="mt-8 flow-root">
@@ -44,54 +43,29 @@
                                                 Email
                                             </th>
                                             <th scope="col"
-                                                class="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-3 pr-4 backdrop-blur backdrop-filter sm:pr-6 lg:pr-8">
-                                                <span class="sr-only">Edit</span>
+                                                class="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 text-left pl-3 pr-4 backdrop-blur backdrop-filter sm:pr-6 lg:pr-8">
+                                                Action
                                             </th>
-                                            <th scope="col"
-                                                class="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-3 pr-4 backdrop-blur backdrop-filter sm:pr-6 lg:pr-8">
-                                                <span class="sr-only">Edit</span>
-                                            </th>
-
                                         </tr>
                                         </thead>
                                         <tbody>
-                                        <tr>
-                                            <td class="whitespace-nowrap border-b border-gray-200 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">
-                                                Ambience
-                                            </td>
-                                            <td class="hidden whitespace-nowrap border-b border-gray-200 px-3 py-4 text-sm text-gray-500 lg:table-cell">
-                                                systems@ambience.com
-                                            </td>
-                                            <td class="relative whitespace-nowrap border-b border-gray-200 py-4 pl-3 pr-4 text-center text-sm font-medium sm:pr-8 lg:pr-8">
-                                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span
-                                                        class="sr-only">, Ambience</span></a>
-                                            </td>
-                                            <td class="relative whitespace-nowrap border-b border-gray-200 py-4 pl-3 pr-4 text-center text-sm font-medium sm:pr-8 lg:pr-8">
-                                                <a href="#" class="text-red-600 hover:text-indigo-900">Delete<span
-                                                        class="sr-only">, Ambience</span></a>
-                                            </td>
-                                        </tr>
+                                        @foreach($organizations as $organization)
+                                            <tr>
+                                                <td class="whitespace-nowrap border-b border-gray-200 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">
+                                                    {{$organization->o_name}}
+                                                </td>
+                                                <td class="hidden whitespace-nowrap border-b border-gray-200 px-3 py-4 text-sm text-gray-500 lg:table-cell">
+                                                    {{$organization->o_email}}
+                                                </td>
+                                                <td class="relative whitespace-nowrap border-b border-gray-200 py-4 pl-3 pr-4 text-center text-sm font-medium sm:pr-8 lg:pr-8">
+                                                    <div class="flex space-x-8">
+                                                        <div><a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a></div>
+                                                        <div><a href="#" class="text-red-600 hover:text-red-900">Delete</a></div>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        @endforeach
 
-                                        <tr>
-                                            <td class="whitespace-nowrap border-b border-gray-200 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">
-                                                Miniso
-                                            </td>
-                                            <td class="hidden whitespace-nowrap border-b border-gray-200 px-3 py-4 text-sm text-gray-500 lg:table-cell">
-                                                systems@miniso.com
-                                            </td>
-                                            <td class="relative whitespace-nowrap border-b border-gray-200 py-4 pl-3 pr-4 text-center text-sm font-medium sm:pr-8 lg:pr-8">
-                                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span
-                                                        class="sr-only">, Miniso</span></a>
-                                            </td>
-                                            <td class="relative whitespace-nowrap border-b border-gray-200 py-4 pl-3 pr-4 text-center text-sm font-medium sm:pr-8 lg:pr-8">
-                                                <a href="#" class="text-red-600 hover:text-indigo-900">Delete<span
-                                                        class="sr-only">, Miniso</span></a>
-                                            </td>
-                                        </tr>
-
-
-
-                                        <!-- More people... -->
                                         </tbody>
                                     </table>
                                 </div>
