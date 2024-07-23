56f235b13dc958422c2d062875e5b3f9b4d99375
Sanduni Fernando <sandunigfdo@gmail.com>
2024-07-24 09:36:37 +1200

    Organize the code

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,11 +11,15 @@
                 </div>
 
                 <!-- Navigation Links -->
+
+                <!-- Customer Management -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Customer Management') }}
                     </x-nav-link>
                 </div>
+
+
             </div>
 
             <!-- Settings Dropdown -->
