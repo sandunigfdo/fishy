62e0afab53b0002809aa017f67bf6e3aca3de8c7
Sanduni Fernando <sandunigfdo@gmail.com>
2024-08-01 00:27:14 +1200

    Add a new relationship between user and employee

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -52,6 +53,10 @@ class User extends Authenticatable
     public function organization():HasOne
     {
         return $this->hasOne(Organization::class);
+    }
+
+    public function emploees():HasMany{
+        return $this->hasMany(Employee::class);
     }
 
 }
