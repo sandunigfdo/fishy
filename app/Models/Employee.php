2d0c5e29cab98e2f7e9ca5c223b490e794897d97
Sanduni Fernando <sandunigfdo@gmail.com>
2024-07-31 22:03:07 +1200

    Specify fillable fields in employees table

---
+++ app/Models/Employee.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Employee extends Model
+{
+    use HasFactory;
+    // Specify the fillable fields for mass assignment
+    protected $fillable = [
+        'f_name',
+        'l_name',
+        'email',
+        'position',
+        'department',
+    ];
+    public function user():HasOne{
+        return $this->hasOne(User::class);
+    }
+}
