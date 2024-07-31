57f315c79909b04d4e36938549e087e7aba56580
Sanduni Fernando <sandunigfdo@gmail.com>
2024-07-31 22:01:49 +1200

    Create employees table

---
+++ database/migrations/2024_07_30_231627_create_employees_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('f_name');
+            $table->string('l_name');
+            $table->string('position');
+            $table->string('department');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
