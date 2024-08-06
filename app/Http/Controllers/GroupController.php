13674e4630e8bd9cbc99c846890ad9ba36e0deae
Sanduni Fernando <sandunigfdo@gmail.com>
2024-08-07 10:55:57 +1200

    Create the controller for groups

---
+++ app/Http/Controllers/GroupController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Group;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+
+class GroupController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index():View
+    {
+        return view('groups.index', [
+            'groups' => Group::all(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $validated = $request->validate([
+            'name' => 'required|unique:groups|max:255',
+        ]);
+
+        // Create a new Group
+        $group = new Group();
+        $group->name = $request->input('name');
+        $group->user_id = Auth::user()->id;
+        $group->save();
+
+        return redirect('/employee_management')->with('success', 'Group created!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Group $group)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Group $group)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Group $group)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Group $group)
+    {
+        //
+    }
+}
