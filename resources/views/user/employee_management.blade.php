6ea2423ec4582a833df512f7a2dd424089625d1a
Sanduni Fernando <sandunigfdo@gmail.com>
2024-08-14 09:57:58 +1200

    Update the column header in the Group Table

--- resources/views/user/employee_management.blade.php
+++ resources/views/user/employee_management.blade.php
@@ -256,7 +256,7 @@
                                             <tr>
                                                 <th scope="col"
                                                     class="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-base font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8">
-                                                    First Name
+                                                    Group Name
                                                 </th>
                                                 <th scope="col"
                                                     class="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 text-left pl-3 pr-4 backdrop-blur backdrop-filter sm:pr-6 lg:pr-8">
