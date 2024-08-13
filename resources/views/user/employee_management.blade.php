4d9f1c90f7af1c8b8cdf8e2fe0067401556d18f1
Sanduni Fernando <sandunigfdo@gmail.com>
2024-08-14 09:42:47 +1200

    Add Employee Group Management section

--- resources/views/user/employee_management.blade.php
+++ resources/views/user/employee_management.blade.php
@@ -185,27 +185,49 @@
                                                             </div>
                                                        @endforeach
 
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-10">
-                <div class="px-4 sm:px-6 lg:px-8">
-                    <div class="sm:flex sm:items-center">
-                        <div class="sm:flex-auto">
-                            <h1 class="text-lg font-semibold leading-6 text-gray-900">Groups</h1>
-                            <p class="mt-2 text-base text-gray-700">Add Employees to groups.</p>
+                                                    </div>
+                                                </fieldset>
+                                            </div>
+                                        </div>
+                                        <div class="flex items-center justify-end gap-x-6 border-gray-900/10 px-4 py-4 sm:px-8">
+                                            <button type="button"
+                                                    class="text-sm font-semibold leading-6 text-gray-900">
+                                                    Cancel
+                                            </button>
+                                            <button type="submit"
+                                                    class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                                    Save
+                                            </button>
+                                        </div>
+                                    </form>
+                                    </div>
+                                    <!-- Multi Select -->
+                                </div>
+                            </div>
                         </div>
-
-{{--                        <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">--}}
-{{--                            <button type="submit"--}}
-{{--                                    class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">--}}
-{{--                                    Add Group--}}
-{{--                            </button>--}}
-{{--                        </div>--}}
+                        <!-- Container -->
                     </div>
-                    <!--  -->
-                    <div class="mt-8 flow-root">
-                        <div class="-mx-4 -my-2 sm:-mx-6 lg:-mx-8">
-                            <div class="inline-block min-w-full py-2 align-middle">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Display Groups -->
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-10">
+                    <div class="px-4 sm:px-6 lg:px-8">
+                        <div class="sm:flex sm:items-center">
+                            <div class="sm:flex-auto">
+                                <h1 class="text-lg font-semibold leading-6 text-gray-900">Groups</h1>
+                                <p class="mt-2 text-base text-gray-700">Add Employees to groups.</p>
+                            </div>
+                        </div>
+                        <!-- Container -->
+                        <div class="mt-8 flow-root">
+                            <div class="-mx-4 -my-2 sm:-mx-6 lg:-mx-8">
+                                <div class="inline-block min-w-full py-2 align-middle">
                                 <!-- Group Form  -->
                                     <form method="POST" action="{{ route('groups.store') }}">
                                         @csrf
