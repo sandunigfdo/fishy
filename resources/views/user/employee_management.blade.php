abe8deb3935d8adf91221a8c872539655f6516d2
Sanduni Fernando <sandunigfdo@gmail.com>
2024-08-14 09:40:37 +1200

    Add a new column to Employee Management table to display group info

--- resources/views/user/employee_management.blade.php
+++ resources/views/user/employee_management.blade.php
@@ -46,6 +46,10 @@
                                                 Department
                                             </th>
                                             <th scope="col"
+                                                class="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-base font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8">
+                                                Group
+                                            </th>
+                                            <th scope="col"
                                                 class="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 text-left pl-3 pr-4 backdrop-blur backdrop-filter sm:pr-6 lg:pr-8">
                                                 Action
                                             </th>
