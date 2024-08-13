11bc946980cc2db587459ea6f684837843d034fd
Sanduni Fernando <sandunigfdo@gmail.com>
2024-08-14 09:38:41 +1200

    Define the relationship with employees and group

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Employee extends Model
@@ -19,5 +20,8 @@ class Employee extends Model
     ];
     public function user():HasOne{
         return $this->hasOne(User::class);
+    }
+    public function group():BelongsTo{
+        return $this->belongsTo(Group::class);
     }
 }
