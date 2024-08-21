f0d536463f25a1ba5450d89110162ff161e90e4c
Sanduni Fernando <sandunigfdo@gmail.com>
2024-08-21 23:36:38 +1200

    Remove CSRF validation

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->validateCsrfTokens(except: [
+        '/canary/webhook',
+        '/employees/input_handle',
+        '/landing',
+    ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
