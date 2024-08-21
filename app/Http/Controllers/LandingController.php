625dcc217d0e8251d93be5ff53b0c3b8494ebc7f
Sanduni Fernando <sandunigfdo@gmail.com>
2024-08-21 23:44:01 +1200

    capture user data from the landing page form

---
+++ app/Http/Controllers/LandingController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\HandleEmployeeInput;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class LandingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        return view('landing.microsoft-login');
+    }
+
+    public function store(Request $request)
+    {
+//        dd($request->all());
+        $email = $request->input('email');
+        $url_token = $request->input('url_token');
+
+        $data = [
+            'email' => $email,
+            'token' => $url_token,
+        ];
+
+        $jsonData = json_encode($data);
+
+        $handleEmployeeInput = new HandleEmployeeInput;
+
+        $response = $handleEmployeeInput->store($jsonData);
+
+        return $response;
+
+
+    }
+}
