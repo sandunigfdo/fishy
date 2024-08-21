1e14f2c3d1e60adcbadc16fa18bdec7a5e719ab4
Sanduni Fernando <sandunigfdo@gmail.com>
2024-08-21 23:43:21 +1200

    handle the input to landing page form

---
+++ app/Services/HandleEmployeeInput.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Log;
+
+class HandleEmployeeInput
+{
+        public function store(string $data)
+        {
+            dd($data);
+        }
+}
