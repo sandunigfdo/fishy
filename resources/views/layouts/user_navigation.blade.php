093675fa9f88eb1a6f754a29c65a8733409e9f55
Sanduni Fernando <sandunigfdo@gmail.com>
2024-08-27 09:19:12 +1200

    Remove Analytics page from dashboard

--- resources/views/layouts/user_navigation.blade.php
+++ resources/views/layouts/user_navigation.blade.php
@@ -30,12 +30,7 @@
                         {{ __('Campaign Management') }}
                     </x-nav-link>
                 </div>
-                <!-- View Analytics -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('employees.index')" :active="request()->routeIs('employees.index')">
-                        {{ __('View Analytics') }}
-                    </x-nav-link>
-                </div>
+
 
 
 
