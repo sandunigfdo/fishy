d9b485616ac6e65026028548ca501dffa311aad7
Sanduni Fernando <sandunigfdo@gmail.com>
2024-08-27 09:26:18 +1200

    Write the logic to display and store campaign info and dynamically display the group info

---
+++ app/Http/Controllers/CampaignController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Campaign;
+use App\Models\Employee;
+use App\Models\Group;
+use App\Models\Results;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+use Illuminate\Http\JsonResponse;
+
+class CampaignController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        return view('campaign.index', [
+            'employees' => Employee::all(),
+            'groups' => Group::all(),
+            'campaigns' => Campaign::all(),
+            'results' => Results::all()
+        ]);
+    }
+
+    public function getEmployeesByGroup(Request $request)
+    {
+        $groupId = $request->input('group_id');
+        // Get the authenticated user
+        $user = Auth::user();
+        // Get employees associated with currently authenticated user that belong to the specified group
+        $employees = $user->employees()->where('group_id', $groupId)->get();
+
+        return response()->json($employees);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+//            'created_date' => 'nullable|date',
+//            'launch_date' => 'nullable|date',
+//            'status' => 'nullable|string|max:255',
+        ]);
+
+        // Create a new campaign
+        $campaign = new Campaign();
+        $campaign->name = $validated['name'];
+        $campaign->created_date = null;
+        $campaign->launch_date = null;
+        $campaign->status = null;
+        $campaign->user_id = Auth::user()->id;
+        $campaign->save();
+
+        return redirect()->route('campaign.index');
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Campaign $campaign)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Campaign $campaign)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Campaign $campaign)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Campaign $campaign)
+    {
+        //
+    }
+}
