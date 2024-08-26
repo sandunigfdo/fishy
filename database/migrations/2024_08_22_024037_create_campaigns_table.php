f2f6a043c5ea27b38f96a4e727804ed5fe606666
Sanduni Fernando <sandunigfdo@gmail.com>
2024-08-27 09:23:12 +1200

    Create the migration for campaigns

---
+++ database/migrations/2024_08_22_024037_create_campaigns_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('campaigns', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->date('created_date')->nullable();
+            $table->date('launch_date')->nullable();
+            $table->string('status')->nullable();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('campaigns');
+    }
+};
