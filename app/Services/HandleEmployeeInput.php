e12f02bb7bedab6a0f0f577903e6f03a3aa1c850
Sanduni Fernando <sandunigfdo@gmail.com>
2024-08-27 09:14:39 +1200

    Handle employee submitted data on the landing page.

--- app/Services/HandleEmployeeInput.php
+++ app/Services/HandleEmployeeInput.php
@@ -2,12 +2,31 @@
 
 namespace App\Services;
 
+use App\Models\Results;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class HandleEmployeeInput
 {
         public function store(string $data)
         {
-            dd($data);
+//            dd($data);
+
+            // Decode the JSON to an associative array
+            $data = json_decode($data, true);
+            // Access the 'token'
+            $token = $data['token'];
+
+           // If the url_token passed, exists in the results table, set the submit_creds to TRUE
+           $found = Results::where('url_token', $token)->exists();
+
+           if($found == true){
+               DB::table('results')
+                  ->where('url_token', $token)
+                  ->update(['submit_creds' => true]);
+           }
+
+           return redirect()->route('campaign.index');
+
         }
 }
