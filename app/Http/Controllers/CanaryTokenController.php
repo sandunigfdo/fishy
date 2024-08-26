9ec36af8cbd409f5e9210c908b581a7ca40867f2
Sanduni Fernando <sandunigfdo@gmail.com>
2024-08-27 09:12:25 +1200

    Handle the request coming from canary app

--- app/Http/Controllers/CanaryTokenController.php
+++ app/Http/Controllers/CanaryTokenController.php
@@ -2,14 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Results;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class CanaryTokenController extends Controller
 {
    public function handleWebhook(Request $request)
    {
-       Log::info($request->all());
+       Log::info($request->input('token'));
+       $token = $request->input('token');
+       // If the request's token exists in the results table, set the click link to TRUE
+       $found = Results::where('canary_id', $token)->exists();
+
+       if($found !== 0){
+           DB::table('results')
+              ->where('canary_id', $token)
+              ->update(['click_link' => true]);
+       }
+
        return "ok";
+
    }
 }
