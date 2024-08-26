4481f41060de1102e90c7038dc284b3e1400f9ec
Sanduni Fernando <sandunigfdo@gmail.com>
2024-08-27 09:17:56 +1200

    Define the relationship between campaigns

--- app/Models/User.php
+++ app/Models/User.php
@@ -60,4 +60,8 @@ class User extends Authenticatable
         return $this->hasMany(Group::class);
     }
 
+    public function campaigns():HasMany{
+        return $this->hasMany(Campaign::class);
+    }
+
 }
