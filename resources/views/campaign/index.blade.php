7150d53143d0e49e209743d868a53fa91b72c415
Sanduni Fernando <sandunigfdo@gmail.com>
2024-09-10 15:07:00 +1200

    Add employee email to canary assignment table

--- resources/views/campaign/index.blade.php
+++ resources/views/campaign/index.blade.php
@@ -224,6 +224,19 @@
                                                                 class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                                                         </div>
                                                     </div>
+                                                    <div class="flex-1">
+                                                        <label :for="'e_email-' + employee.id"
+                                                            class="block text-sm font-medium leading-6 text-gray-900">
+                                                            Employee
+                                                        </label>
+                                                        <div class="mt-2">
+                                                            <input type="text"
+                                                                :name="'e_email-' + employee.id"
+                                                                :id="'e_email-' + employee.id"
+                                                                x-bind:value="employee.email"
+                                                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                                        </div>
+                                                    </div>
                                                 </div>
                                             </template>
                                         </div>
