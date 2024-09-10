babe046c8e8c3b6442422fffc7bb87d18bc86246
Sanduni Fernando <sandunigfdo@gmail.com>
2024-09-10 15:09:13 +1200

    Implement the logic to send an email

---
+++ app/Mail/EmailCampaign.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Results;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Campaign;
+use App\Models\Employee;
+
+
+class EmailCampaign extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(
+        public Employee $employee,
+        public Campaign $campaign,
+        public Results $results,
+    ) { }
+
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            from: new Address('noreply@trial-3zxk54vkx5qljy6v.mlsender.net', 'noreply'),
+            subject: 'Email Campaign',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'email.template',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
