3b1d905f5932c0cd62bcfe93dc3d3b468496390a
Sanduni Fernando <sandunigfdo@gmail.com>
2024-09-10 14:52:34 +1200

    Add /send_email endpoint to csrf except

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         '/canary/webhook',
         '/employees/input_handle',
         '/landing',
+        '/send_email'
     ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
