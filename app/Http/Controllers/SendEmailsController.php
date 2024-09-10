8411f4c35c3976a4d337a586b6b25e435a8cb2cc
Sanduni Fernando <sandunigfdo@gmail.com>
2024-09-10 15:09:34 +1200

    Implement the logic to send an email

---
+++ app/Http/Controllers/SendEmailsController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\EmailCampaign;
+use Illuminate\Http\RedirectResponse;
+use Symfony\Component\Mailer\Exception\TransportException;
+
+
+class SendEmailsController extends Controller
+{
+    public function store()
+    {
+        $recipient = 'lupwwnsamie@gmail.com';
+        try {
+             Log::info('Starting to send email');
+             Mail::to($recipient)->send(new EmailCampaign());
+             Log::info('Email sent to: ' . $recipient);
+        } catch (TransportException $e) {
+            Log::error($e->getMessage());
+        }
+
+
+         return back()->with('success', 'Test email sent successfully!');
+
+
+
+    }
+}
