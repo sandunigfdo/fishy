411708ee5135893b4f44139cdb76caee166cd74a
Sanduni Fernando <sandunigfdo@gmail.com>
2024-09-21 16:02:53 +1200

    Cleanup

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\CanaryTokenController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\EmployeeGroupController;
-use App\Http\Controllers\EmployeeInputHandleController;
 use App\Http\Controllers\EmployeeManagementController;
 use App\Http\Controllers\GroupController;
 use App\Http\Controllers\LandingController;
@@ -15,7 +14,6 @@ use App\Http\Controllers\SendEmailsController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserDashboardController;
-use App\Http\Middleware\EnsureAdmin;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisteredUserController;
 
@@ -23,17 +21,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Route::get('/dashboard', function () {
-//    return view('dashboard');
-//})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::middleware('auth', EnsureAdmin::class)->group(function () {
+Route::middleware('auth')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('/users', UserController::class)->only(['index', 'store', 'edit', 'update', 'destroy']);
