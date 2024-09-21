36c6910d560d9e463607e47a5048cf98452afbac
Sanduni Fernando <sandunigfdo@gmail.com>
2024-09-22 10:17:37 +1200

    Change "customer" to "user" and cleanup

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,30 +1,20 @@
 <x-app-layout>
-{{--    <x-slot name="header">--}}
-{{--        <h2 class="font-semibold text-xl text-gray-800 leading-tight">--}}
-{{--            {{ __('Dashboard') }}--}}
-{{--        </h2>--}}
-{{--    </x-slot>--}}
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-{{--            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">--}}
-{{--                <div class="p-6 text-gray-900">--}}
-{{--                    {{ __("You're logged in!") }}--}}
-{{--                </div>--}}
-{{--            </div>--}}
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-10">
-                    <!-- Customer Management table -->
+                    <!-- User Management table -->
                     <div class="px-4 sm:px-6 lg:px-8">
                         <div class="sm:flex sm:items-center">
                             <div class="sm:flex-auto">
-                                <h1 class="text-lg font-semibold leading-6 text-gray-900">Customers</h1>
-                                <p class="mt-2 text-base text-gray-700">A list of all Customers.</p>
+                                <h1 class="text-lg font-semibold leading-6 text-gray-900">User</h1>
+                                <p class="mt-2 text-base text-gray-700">A list of all current users.</p>
                             </div>
                             <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
                                 <a href="{{ route('users.index') }}"
                                    class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                                    Add Customer
+                                    Add User
                                 </a>
                             </div>
                         </div>
@@ -52,7 +42,7 @@
                                         @foreach($users as $user)
                                             <tr>
                                                 <td class="whitespace-nowrap border-b border-gray-200 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">
-                                                    {{$user->organization}}
+                                                    {{$user->name}}
                                                 </td>
                                                 <td class="hidden whitespace-nowrap border-b border-gray-200 px-3 py-4 text-sm text-gray-500 lg:table-cell">
                                                     {{$user->email}}
@@ -80,7 +70,7 @@
                         </div>
                     </div>
 
-                    <!-- Customer Management Table -->
+                    <!-- User Management Table -->
                 </div>
             </div>
 
