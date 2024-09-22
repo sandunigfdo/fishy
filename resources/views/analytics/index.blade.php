feda72beb02f21c7061abb5fed191e925a88bc1c
Sanduni Fernando <sandunigfdo@gmail.com>
2024-09-22 16:56:22 +1200

    Cleanup

--- resources/views/analytics/index.blade.php
+++ resources/views/analytics/index.blade.php
@@ -9,7 +9,7 @@
                         <div class="sm:flex sm:items-center">
                             <div class="sm:flex-auto">
                                 <h1 class="text-lg font-semibold leading-6 text-gray-900">Results</h1>
-                                <p class="mt-2 text-base text-gray-700">Results of the {{$campaign->name}} Campaign.</p>
+                                <p class="mt-2 text-base text-gray-700">Results of the {{ $campaign->name }} Campaign.</p>
                             </div>
                         </div>
                         <div class="mt-8 flow-root">
@@ -20,10 +20,6 @@
                                         <table class="min-w-full border-separate border-spacing-0">
                                             <thead>
                                             <tr>
-                                                <th scope="col"
-                                                    class="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-base font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8">
-                                                    Employee Name
-                                                </th>
                                                 <th scope="col"
                                                     class="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-base font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8">
                                                     Email
@@ -38,6 +34,10 @@
                                                 </th>
                                                 <th scope="col"
                                                     class="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-base font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8">
+                                                    Group
+                                                </th>
+                                                <th scope="col"
+                                                    class="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-base font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8">
                                                     Link Clicked
                                                 </th>
                                                 <th scope="col"
@@ -47,62 +47,52 @@
                                             </tr>
                                             </thead>
                                             <tbody>
-                                            @foreach($employees as $employee)
+                                            @foreach($results as $result)
                                                 <tr>
                                                     <td class="whitespace-nowrap border-b border-gray-200 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">
-                                                        {{$employee->f_name}} {{$employee->l_name}}
+                                                        {{ $result->employee->email }}
                                                     </td>
                                                     <td class="whitespace-nowrap border-b border-gray-200 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">
-                                                        {{$employee->email}}
+                                                        {{ $result->employee->position }}
                                                     </td>
                                                     <td class="whitespace-nowrap border-b border-gray-200 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">
-                                                        {{$employee->position}}
+                                                        {{ $result->employee->department }}
                                                     </td>
                                                     <td class="whitespace-nowrap border-b border-gray-200 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">
-                                                        {{$employee->department}}
-                                                    </td>
-                                                    <td class="whitespace-nowrap border-b border-gray-200 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">
-                                                        <!-- Display results related to this employee -->
-                                                        @if(isset($employee_results[$employee->id]) && count($employee_results[$employee->id]) > 0)
-                                                            @foreach($employee_results[$employee->id] as $result)
-                                                                <div>
-                                                                    @if($result->click_link == 1)
-                                                                        <span class="inline-flex items-center rounded-md bg-yellow-100 px-1.5 py-0.5 text-xs font-medium text-yellow-700">clicked link</span>
-                                                                    @endif
-
-                                                                </div>
-                                                            @endforeach
-                                                        @else
-                                                            <p>No results found</p>
-                                                        @endif
+                                                        {{ $result->employee->group->name }}
                                                     </td>
                                                     <td class="whitespace-nowrap border-b border-gray-200 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">
                                                         <!-- Display results related to this employee -->
-                                                        @if(isset($employee_results[$employee->id]) && count($employee_results[$employee->id]) > 0)
-                                                            @foreach($employee_results[$employee->id] as $result)
-                                                                <div>
-                                                                    @if($result->submit_creds == 1)
-                                                                        <span class="inline-flex items-center rounded-md bg-red-100 px-1.5 py-0.5 text-xs font-medium text-red-700">submitted data</span>
-                                                                    @endif
-
-                                                                </div>
-                                                            @endforeach
+                                                        @if($result->click_link)
+                                                            <span class="inline-flex items-center rounded-md bg-yellow-100 px-1.5 py-0.5 text-xs font-medium text-yellow-700">
+                                                                Link clicked
+                                                            </span>
                                                         @else
-                                                            <p>No results found</p>
+                                                            <span class="inline-flex items-center rounded-md bg-green-100 px-1.5 py-0.5 text-xs font-medium text-green-700">
+                                                                Link not clicked
+                                                            </span>
+                                                        @endif
+
+                                                    </td>
+                                                    <td class="whitespace-nowrap border-b border-gray-200 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">
+                                                        <!-- Display results related to this employee -->
+                                                        @if($result->submit_creds)
+                                                            <span class="inline-flex items-center rounded-md bg-red-100 px-1.5 py-0.5 text-xs font-medium text-red-700">
+                                                                Data submitted
+                                                            </span>
+                                                        @else
+                                                            <span class="inline-flex items-center rounded-md bg-green-100 px-1.5 py-0.5 text-xs font-medium text-green-700">
+                                                                Data not submitted
+                                                            </span>
                                                         @endif
                                                     </td>
-
-
                                                 </tr>
                                             @endforeach
 
 
                                             </tbody>
                                         </table>
-
                                         <!-- Campaign Management Table -->
-
-
                                     </div>
                                 </div>
                             </div>
