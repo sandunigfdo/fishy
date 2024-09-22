f28b22996cc570e8f1c04b40773c1ca38623d1d7
Sanduni Fernando <sandunigfdo@gmail.com>
2024-09-22 23:39:19 +1200

    Update button

--- resources/views/user/userdashboard.blade.php
+++ resources/views/user/userdashboard.blade.php
@@ -43,7 +43,7 @@
                                                 </p>
                                                 <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
                                                     <a href="{{ route('analytics.index', $campaign->id) }}"
-                                                       class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                                       class="text-orange-600 hover:text-orange-900">
                                                         View Results
                                                     </a>
                                                 </div>
