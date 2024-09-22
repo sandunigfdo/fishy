ff14a51ee3a10b58556fbfb5bd7604e3571f7ba6
Sanduni Fernando <sandunigfdo@gmail.com>
2024-09-22 22:04:55 +1200

    Update wording

--- resources/views/campaign/index.blade.php
+++ resources/views/campaign/index.blade.php
@@ -244,7 +244,7 @@
                                 <a href="{{ route('campaign.index') }}" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
                                 <button type="submit"
                                     class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                                    Issue
+                                    Assign
                                 </button>
                             </div>
                         </form>
