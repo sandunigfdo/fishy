78ce1c6f2ae98e839cec5d6b39120e4f360f7acb
Sanduni Fernando <sandunigfdo@gmail.com>
2024-09-22 16:56:49 +1200

    Fix relationship with employee

--- app/Models/Result.php
+++ app/Models/Result.php
@@ -5,9 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Results extends Model
+class Result extends Model
 {
     use HasFactory;
     protected $fillable = [
@@ -18,8 +17,9 @@ class Results extends Model
         'canary_id',
     ];
 
-    public function employee():HasMany{
-        return $this->HasMany(Employee::class);
+    public function employee(): BelongsTo
+    {
+        return $this->belongsTo(Employee::class);
     }
 
     public function campaign(): BelongsTo
