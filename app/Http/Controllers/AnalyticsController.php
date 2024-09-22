25453fa4a4ee0fb7f9b8b8e37f733606509d2991
Sanduni Fernando <sandunigfdo@gmail.com>
2024-09-22 16:55:55 +1200

    Simplify the query

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -3,12 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Campaign;
-use App\Models\User;
-use App\Models\Employee;
-use App\Models\Group;
-use App\Models\Results;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use App\Models\Result;
 use Illuminate\View\View;
 
 
@@ -17,36 +12,16 @@ class AnalyticsController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index($campaign): View
+    public function index(Campaign $campaign): View
     {
-        // Get the campaign record
-        $campaign = DB::table('campaigns')->where('id', $campaign)->first();
-        // Get the results associated with the campaign
-        $results = DB::table('results')->where('campaign_id', $campaign->id)->get();
-        // Get the employees of associated with the campaign
-        $employees = [];
-        $employee_results = [];
-        foreach ($results as $result) {
-            $employeeId = $result->employee_id;
-            $employee = DB::table('employees')->where('id', $employeeId)->first();
-            if ($employee) {
-                // Store employee details
-                $employees[$employeeId] = $employee;
-                // Initialize array for employee results if not already set
-                if (!isset($employeeResults[$employeeId])) {
-                    $employee_results[$employeeId] = [];
-                }
-                // Store result details for this employee
-                $employee_results[$employeeId][] = $result;
-            }
-
-        }
-//        dd($employee_results);
+        $results = Result::query()
+            ->where('campaign_id', $campaign->id)
+            ->with('employee.group')
+            ->get();
 
         return view('analytics.index', [
-            'employees' => $employees,
             'campaign' => $campaign,
-            'employee_results' => $employee_results,
+            'results' => $results,
         ]);
     }
 }
