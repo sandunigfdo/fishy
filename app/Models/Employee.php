2ef9eb8433a49b1161733246dc4fd0471e2a7429
Sanduni Fernando <sandunigfdo@gmail.com>
2024-09-22 16:56:10 +1200

    Add relationship with results

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Employee extends Model
@@ -30,4 +31,10 @@ class Employee extends Model
     {
         return $this->belongsTo(Group::class);
     }
+
+    public function results(): HasMany
+    {
+        return $this->hasMany(Result::class);
+    }
+
 }
