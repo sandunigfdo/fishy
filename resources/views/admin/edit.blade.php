6c40d4cc4cf9664ea48ac7bb09b809e5324575ad
Sanduni Fernando <sandunigfdo@gmail.com>
2024-09-24 00:12:39 +1200

    Change button colour

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -79,7 +79,7 @@
                                                 Cancel
                                         </a>
                                         <button type="submit"
-                                                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                                class="rounded-md bg-gray-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                                                 Save
                                         </button>
                                     </div>
