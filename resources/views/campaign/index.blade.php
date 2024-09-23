d2fc33275dde49dc801f38b681cbd4a4c51f4e0d
Sanduni Fernando <sandunigfdo@gmail.com>
2024-09-24 00:38:22 +1200

    Add a placeholder value

--- resources/views/campaign/index.blade.php
+++ resources/views/campaign/index.blade.php
@@ -134,7 +134,11 @@
                                         <div class="sm:col-span-6">
                                             <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Name</label>
                                             <div class="mt-2">
-                                                <input id="name" name="name" type="text" required
+                                                <input id="name"
+                                                       name="name"
+                                                       type="text"
+                                                       placeholder="Create a new Campaign"
+                                                       required
                                                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                                             </div>
                                         </div>
