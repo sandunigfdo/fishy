c221d282ef40e790e30673cb8f1d5613b4ae0c6d
Sanduni Fernando <sandunigfdo@gmail.com>
2024-09-24 00:13:46 +1200

    Change the colour of button

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -89,8 +89,8 @@
                                         Cancel
                                     </a>
                                     <button type="submit"
-                                            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                                        Save
+                                            class="rounded-md bg-gray-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                            Save
                                     </button>
                                 </div>
                             </form>
