692d6b430f1f09875af391b88495eec04514f1b9
Sanduni Fernando <sandunigfdo@gmail.com>
2024-09-24 00:17:40 +1200

    Add the route to Cancel button

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -84,7 +84,7 @@
                                     </div>
                                 </div>
                                 <div class="mt-6 flex items-center justify-end gap-x-6">
-                                    <a  href="#"
+                                    <a  href="{{ route('users.index') }}"
                                         class="text-sm font-semibold leading-6 text-gray-900">
                                         Cancel
                                     </a>
