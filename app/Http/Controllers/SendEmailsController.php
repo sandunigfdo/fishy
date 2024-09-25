0744209187f85a4cda6257c9fb90c0f9a8db215a
Sanduni Fernando <sandunigfdo@gmail.com>
2024-09-25 23:22:45 +1200

    Send email

--- app/Http/Controllers/SendEmailsController.php
+++ app/Http/Controllers/SendEmailsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Result;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\EmailCampaign;
@@ -10,19 +12,37 @@ use Symfony\Component\Mailer\Exception\TransportException;
 
 class SendEmailsController extends Controller
 {
-    public function store()
+    public function store(Request $request)
     {
-        $recipient = 'lupwwnsamie@gmail.com';
-        try {
-             Log::info('Starting to send email');
-             Mail::to($recipient)->send(new EmailCampaign());
-             Log::info('Email sent to: ' . $recipient);
-        } catch (TransportException $e) {
-            Log::error($e->getMessage());
+        $campaignId = $request->input('campaign_id');
+
+        // Get users who belong to this campaign
+        $results = Result::query()
+            ->where('campaign_id', $campaignId)
+            ->with('employee')
+            ->get();
+
+        foreach ($results as $result) {
+            try {
+                Log::info('Sending email', ['email' => $result->employee->email]);
+
+                Mail::to($result->employee->email)->send(new EmailCampaign(
+                    $result->employee->f_name,
+                    $result->email_url
+                ));
+
+                // Set email sent status
+                $result->email_sent = true;
+                $result->save();
+
+                Log::info('Email sent');
+
+            } catch (TransportException $e) {
+                Log::error('Sending email failed', ['exception' => $e]);
+            }
         }
 
-
-         return back()->with('success', 'Test email sent successfully!');
+        return back()->with('success', 'Email sent successfully!');
 
 
 
